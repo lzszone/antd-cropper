dbe377ae4923ac09d0688e553c7606ce146417d9
LiZheng <lzszone@gmail.com>
2018-09-30 12:21:35 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,30 +10,60 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.less$/,
+        use: [
+          {
+            loader:'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
+            }
+          }
+        ] 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
         test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/env", "@babel/react"]
+            presets: ["@babel/env", "@babel/react"],
+            plugins: [
+              ["import", { "libraryName": "antd", style: true }]
+            ]
           }
         }
       }
     ]
   },
   mode: 'production',
-  externals: {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom'
-    },
-    antd: 'antd'
-  }
+    /^antd[.]*/
+  ]
 }
